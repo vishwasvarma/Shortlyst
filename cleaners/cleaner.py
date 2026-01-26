8c88827e4cd93cc7d47ad654835e2be394bd410d
Vishwas <vishwasvarma05@gmail.com>
2026-01-26 17:07:53 +0530

    starting a static page

--- cleaners/cleaner.py
+++ cleaners/cleaner.py
@@ -1,19 +1,21 @@
-
 import re
 
-def clean_text(text: str) -> str:
+def extract_and_clean_text(text: str) -> str:
+    """
+    Takes raw text (JD or Resume) and returns cleaned text.
+    """
+
     if not text:
         return ""
-    #lowercases the text
     text = text.lower()
 
-    # replace the new line and tabs with space
-    text = text.replace("\n", " ").replace("\t", " ")
+    text = text.replace("\n", " ")
+    text = text.replace("\t", " ")
 
-    #remove the syombols
     text = re.sub(r"[^a-z0-9+.# ]", " ", text)
 
 
     text = re.sub(r"\s+", " ", text)
+
 
     return text.strip()
